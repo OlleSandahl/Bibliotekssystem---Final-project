8faead34e09d5555e6b6a0993c12a46ec7c366d6
olle sandahl <olle.sandahl@live.se>
2020-08-15 13:41:47 +0200

    Add project files.

---
+++ BusinessEntity/ClassModels/Activity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntity.ClassModels
+{
+    public class Activity
+    {
+        public int ActivityID { get; set; }
+        public string Event { get; set; }
+        public string Adress { get; set; }
+        public string Kategori { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
